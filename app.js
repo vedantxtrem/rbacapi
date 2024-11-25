3c1e957ed14df0dc31d9f70a21d514954b3fe6c3
vedant <vedant@ssipmt.com>
2024-11-25 09:00:22 +0530

    added path

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // app.use(cors({}));
 app.use(cors({
-  origin: ["http://localhost:5173","*"],
+  origin: ["http://localhost:5173","*","https://rbac-frontend-kappa.vercel.app/"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
 }));
