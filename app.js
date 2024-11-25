92eadd34fe459a07e0295fac52c7d8168cc1ce1c
vedant <vedant@ssipmt.com>
2024-11-25 08:52:28 +0530

    fix

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // app.use(cors({}));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173","*"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
 }));
